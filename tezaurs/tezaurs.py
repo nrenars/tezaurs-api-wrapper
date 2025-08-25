2929e09fb8fe2f864c9b35a5ee8f5dd8bea8bb54
nrenars <114558077+nrenars@users.noreply.github.com>
2025-08-25 18:02:43 +0300

    added docstrings

--- tezaurs/tezaurs.py
+++ tezaurs/tezaurs.py
@@ -23,7 +23,7 @@ class Tezaurs:
             word (str): Vārds, ko analizēt.
             lang (Optional[Literal['en']]): Atbildes valoda 'lv' (pēc noklusējuma) vai 'en'.
 
-        Returns:
+        Atgrieztais:
             List[Dict]: Morfoloģiskās analīzes rezultāti JSON formātā.
         """
         if not isinstance(word, str):
@@ -48,6 +48,16 @@ class Tezaurs:
 
     
     def analyze_sentence(self, sentence: str) -> List[Dict]:
+        """
+        Analizē teikumu, izmantojot Tezaura API.
+
+        Izsauc `/analyze_sentence` servisu un atgriež informāciju par katru vārdu teikumā.
+
+        Argumenti:
+            sentence (str): Teikums, ko analizēt.
+        Atgrieztais:
+            List[Dict]: Analīzes rezultāti JSON formātā.
+        """
         sentence = sentence.replace(' ', '%20')
         url = f'{self.url}/analyzesentence/{sentence}'
 
@@ -64,6 +74,23 @@ class Tezaurs:
             raise TezaursNetworkError(f"Tīkla kļūda: {e}")
 
     def inflect(self, word: str, format: Optional[Literal['xml']] = 'json', lang: Optional[Literal['en']] = None) -> List[Dict]:
+        """
+            Izloka vārdu, izmantojot Tezaura API.
+
+            Izsauc `/inflect` servisu un atgriež dotā vārda formas izvēlētajā formātā.
+
+            Argumenti:
+                word (str): Vārds, kuru nepieciešams locīt.
+                format (Optional[Literal['xml']]): Atbildes formāts ('json' pēc noklusējuma vai 'xml').
+                lang (Optional[Literal['en']]): Atbildes valoda 'lv' (pēc noklusējuma) vai 'en'.
+
+            Atgrieztais:
+                List[Dict]: Locījumu formas JSON formātā vai XML (ja izvēlēts).
+
+            Kļūdas:
+                TezaursJSONError: Ja serveris neatgriež derīgu JSON.
+                TezaursNetworkError: Ja notiek tīkla kļūda.
+        """
         if lang is not None:
             url = f'{self.url}/inflect/{format}/{lang}/{word}'
         else:
@@ -84,6 +111,21 @@ class Tezaurs:
 
         
     def inflect_phrase(self, phrase: str, category: Optional[Literal['person', 'org', 'loc']] = None) -> List[Dict]:
+        """
+        Izloka frāzi (piem., īpašvārdus), izmantojot Tezaura API.
+
+        Izsauc `/inflect_phrase` servisu un atgriež frāzes locījumus.
+
+        Argumenti:
+            phrase (str): Frāze, kuru nepieciešams locīt.
+            category (Optional[Literal['person', 'org', 'loc']]): Kategorija
+                - 'person' cilvēku vārdiem,
+                - 'org' organizācijām,
+                - 'loc' vietvārdiem.
+
+        Atgrieztais:
+            List[Dict]: Locījumu formas JSON formātā.
+        """
         phrase.strip("%20") 
         url = f'{self.url}/inflect_phrase/{phrase}'
         if category is not None:
@@ -92,6 +134,23 @@ class Tezaurs:
         return response.json()
     
     def inflect_people(self, name: str, format: Optional[Literal['xml']] = 'json', gender: Optional[Literal['m', 'f']] = None) -> List[Dict]:
+        """
+        Izloka personvārdus (cilvēku vārdus un uzvārdus).
+
+        Izsauc `/inflect_people` servisu un atgriež dotā vārda locījumus.
+
+        Argumenti:
+            name (str): Vārds, uzvārds, vārds un uzvārds, kuru nepieciešams locīt.
+            format (Optional[Literal['xml']]): Atbildes formāts ('json' pēc noklusējuma vai 'xml').
+            gender (Optional[Literal['m','f']]): Dzimums ('m' – vīriešu, 'f' – sieviešu).
+
+        Atgrieztais:
+            List[Dict]: Locījumu formas JSON vai XML formātā.
+
+        Kļūdas:
+            TezaursJSONError: Ja serveris neatgriež derīgu JSON.
+            TezaursNetworkError: Ja notiek tīkla kļūda.
+        """
         url =  f'{self.url}/inflect_people/{format}/{name}'
         if gender is not None:
             url += f'?gender={gender}'
@@ -111,7 +170,24 @@ class Tezaurs:
 
 
     def inflections(self, word: str, paradigm: Optional[Literal['noun-4f', 'verb-1']] = None, stems: Tuple[str, str, str] = None) -> List[Dict]:
-        url = f'{self.url}/v1/inflections/{word}'
+        """
+        Atrod vārda visas iespējamās formas.
+
+        Izsauc `/inflections` servisu, kas atgriež visus locījumus un konjugācijas.
+
+        Argumenti:
+            word (str): Vārds, kuru analizēt.
+            paradigm (Optional[Literal['noun-4f', 'verb-1']]): Paradigmas tips (ja zināms).
+            stems (Tuple[str, str, str]): Trīs saknes formas, ja nepieciešamas.
+
+        Atgrieztais:
+            List[Dict]: Vārda locījumu formas JSON formātā.
+
+        Kļūdas:
+            TezaursJSONError: Ja serveris neatgriež derīgu JSON.
+            TezaursNetworkError: Ja notiek tīkla kļūda.
+        """
+        url = f'{self.url}/{self.version}/inflections/{word}'
         if paradigm is not None:
             url += f'?paradigm={paradigm}'
             if stems is not None:
@@ -127,6 +203,21 @@ class Tezaurs:
             raise TezaursNetworkError(f"Tīkla kļūda: {e}")
 
     def normalize(self, phrase: str, category: Optional[Literal['person', 'org', 'loc']] = None) -> List[Dict]:
+        """
+        Normalizē frāzi (piemēram, īpašvārdus).
+
+        Izsauc `/normalize_phrase` servisu un atgriež frāzes normalizēto formu.
+
+        Argumenti:
+            phrase (str): Frāze, kuru normalizēt.
+            category (Optional[Literal['person', 'org', 'loc']]): Kategorija
+                - 'person' cilvēku vārdiem,
+                - 'org' organizācijām,
+                - 'loc' vietvārdiem.
+
+        Atgrieztais:
+            str: Normalizēta frāze (teksts).
+        """
         phrase.strip("%20")
         url = f'{self.url}/normalize_phrase/{phrase}'
         if category is not None:
@@ -142,6 +233,17 @@ class Tezaurs:
             raise TezaursNetworkError(f"Tīkla kļūda: {e}")
 
     def suitable_paradigm(self, word: str) -> List[Dict]:
+        """
+        Nosaka atbilstošās paradigmas vārdam.
+
+        Izsauc `/suitable_paradigm` servisu un atgriež iespējamos paradigmu tipus.
+
+        Argumenti:
+            word (str): Vārds, kam noteikt paradigmu.
+
+        Atgrieztais:
+            List[Dict]: Saraksts ar iespējamiem paradigmu ID un aprakstiem.
+        """
         url = f'{self.url}/suitable_paradigm/{word}'
         
         try:
@@ -154,6 +256,17 @@ class Tezaurs:
             raise TezaursNetworkError(f"Tīkla kļūda: {e}")
 
     def morphotagger(self, phrase: str) -> List[Dict]:
+        """
+        Veic morfoloģisko frāzes marķēšanu.
+
+        Izsauc `/morphotagger` servisu un atgriež katra vārda morfoloģisko marķējumu.
+
+        Argumenti:
+            phrase (str): Frāze vai teikums, ko analizēt.
+
+        Atgrieztais:
+            List[Dict]: Marķēšanas rezultāti JSON formātā.
+        """
         url = f'{self.url}/morphotagger/{phrase}'
 
         try:
